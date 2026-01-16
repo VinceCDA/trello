27c7499c5427ee2303c1e2c278b7e908babd3d94
VinceCDA <84965037+VinceCDA@users.noreply.github.com>
2026-01-16 12:56:32 +0100

    updated fixtures for user test

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,31 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Utilisateur;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
+        $user = new Utilisateur();
+        $user->setEmail('testuser@test.local');
+
+        $password = $this->hasher->hashPassword($user, 'pass_1234');
+        $user->setPassword($password);
+
+        $manager->persist($user);
+        $manager->flush();
 
         $manager->flush();
     }
